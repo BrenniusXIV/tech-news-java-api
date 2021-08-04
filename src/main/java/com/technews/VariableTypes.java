cab7de4656b014450ae990d7a9f23c285425233a
Brent B <brentbaughan1@gmail.com>
2021-08-03 21:46:59 -0400

    began writing User model class

---
+++ src/main/java/com/technews/VariableTypes.java
@@ -0,0 +1,16 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args){
+        int a = 4;
+        double b = 3.5;
+        double sum = a + b;
+        String z = "dog";
+
+        String stringSum = z + a;
+
+        System.out.println(sum);
+        System.out.println(stringSum);
+
+    }
+}
